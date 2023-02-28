1b873bcaf5bcc8306a986f3d60811e654ea19177
Maxwell Swadling <maxwellswadling@gmail.com>
2023-02-28 20:48:52 +1000

    Initial Commit

---
+++ HardPass/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HardPass
+//
+//  Created by Maxwell on 28/02/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
